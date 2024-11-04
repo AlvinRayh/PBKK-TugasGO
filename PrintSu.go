804c17189212b47d49a36b4d4cd337296385b01d
AlvinRayh <144695510+AlvinRayh@users.noreply.github.com>
2024-11-04 14:36:18 +0700

    Add files via upload

---
+++ PrintSu.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func reverse(s string) string {
+	runes := []rune(s)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return string(runes)
+}
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Masukkan nama (min 3 kata): ")
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println("Input tidak valid")
+		return
+	}
+
+	input = strings.TrimSpace(input)
+	words := strings.Fields(input)
+	if len(words) < 3 {
+		fmt.Println("Input harus minimal 3 kata.")
+		return
+	}
+
+	for _, word := range words {
+		fmt.Println(reverse(word))
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module TugasGO
+
+go 1.23.2
